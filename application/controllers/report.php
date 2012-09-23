f358c45b244e9c225d841a577519b89a38288a32
Fugiman <Fugiman47@gmail.com>
2012-09-23 18:22:12 -0400

    Allow viewing of reports

--- application/controllers/report.php
+++ application/controllers/report.php
@@ -2,6 +2,22 @@
 
 class Report extends CI_Controller {
 
+    public function view($id) {
+        $report = new BugReport($id);
+        if(!$report->exists()) {
+            redirect("/report/list");
+        }
+        
+        $data = array(
+            "ip" => $report->ip,
+            "referrer" => $report->referrer,
+            "canvas" => "data:image/png;base64,".$report->canvas,
+            "debugger" => $this->_json_dump($report->debugger),
+            "report" => $report->report
+        );
+        $this->load->view("report",$data);
+    }
+    
     public function version1() {
         header("Access-Control-Allow-Origin: *");
         $this->load->library('user_agent');
@@ -20,6 +36,49 @@ class Report extends CI_Controller {
             $out["errors"] = $report->error->all;
         }
         print(json_encode($out));
+    }
+    
+    
+    private function _json_value($v) {
+        $output = "";
+        if(is_array($v) || is_object($v)) {
+            $output .= $this->_json_dump($v);
+        } else if(is_string($v)) {
+            $output .= "<span class='json_string'>$v</span>";
+        } else if(is_bool($v)) {
+            $output .= "<span class='json_bool'>".($v?"true":"false")."</span>";
+        } else if(is_null($v)) {
+            $output .= "<span class='json_null'>null</span>";
+        } else if(is_numeric($v)) {
+            $output .= "<span class='json_number'>$v</span>";
+        } else {
+            $output .= "<span class='json_unknown'>unknown</span>";
+        }
+        return $output;
+    }
+
+    private function _json_dump($obj) {
+        $output = "";
+        if(is_object($obj)) {
+            $output .= "<span class='json_object'>Object<ul>";
+            foreach($obj as $k => $v) {
+                $output .= "<li><strong>$k</strong>: ";
+                $output .= $this->_json_value($v);
+                $output .= "</li>";
+            }
+        } else if(is_array($obj)) {
+            $len = count($obj);
+            $output .= "<span class='json_array'>Array[" . $len . "]<ul>";
+            for($k = 0; $k < $len; $k++) {
+                $output .= "<li><strong>$k</strong>: ";
+                $output .= $this->_json_value($obj[$k]);
+                $output .= "</li>";
+            }
+        } else {
+            $output .= "<span class='json_unknown'>Unknown<ul>";
+        }
+        $output .= "</ul></span>";
+        return $output;
     }
 }
 
---
+++ application/views/report.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Bug Report #<?php echo $id; ?></title>
+        <style>.json_string:before, .json_string:after { content: '"'; }</style>
+    </head>
+    <body>
+        <h1><?php echo $ip; ?> at <?php echo $referrer; ?></h1>
+        <img src="<?php echo $canvas; ?>"><br>
+        <hr>
+        <p><?php echo $report; ?></p>
+        <hr>
+        <p><?php echo $debugger; ?></p>
+    </body>
+</html>
