9a5216f0d231f533a035f918d00d6d4528bfa3eb
Fugiman <Fugiman47@gmail.com>
2012-09-24 01:45:38 -0400

    Transposed table fields

--- application/views/reportlist.php
+++ application/views/reportlist.php
@@ -21,9 +21,9 @@
                 <tr>
                     <td><?php echo $report->id; ?></td>
                     <td><?php echo $report->ip; ?></td>
-                    <td><?php echo htmlspecialchars($report->referrer); ?></td>
                     <td><?php echo htmlspecialchars($report->browser); ?></td>
                     <td><?php echo htmlspecialchars($report->os); ?></td>
+                    <td><?php echo htmlspecialchars($report->referrer); ?></td>
                     <td><?php echo htmlspecialchars($report->report); ?></td>
                     <td><a href="/report/view/<?php echo $report->id; ?>">View</a></td>
                 </tr>
